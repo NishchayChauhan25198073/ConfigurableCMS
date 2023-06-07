3388e6968e5311a42426911cd123119f5cbff9f5
Nishchay Chauhan <nishchay.chauhan@indialends.com>
2023-06-07 18:21:17 +0530

    local commit

---
+++ src/UI/CMSSystemComponents/Components/PageEntry.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using CMS.Foundation.Infrastructure.Types;
+
+namespace CMS.Client.UI.CMSComponents
+{
+    public partial class PageEntry
+    {
+        public CMS.Foundation.Infrastructure.Types.PageItem ContextPageItem { get; set; }
+    }
+}
